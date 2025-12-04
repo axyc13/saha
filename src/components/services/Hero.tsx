55e7dae8cc5443ce93aa842b236b9b499140220b
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-12-04 18:47:36 +1300

    feat: service overview responsive

--- src/components/services/Hero.tsx
+++ src/components/services/Hero.tsx
@@ -4,7 +4,7 @@ import placeholder from "@/public/placeholder.jpg";
 
 export default function Hero() {
   return (
-    <section className="w-screen h-[72vh] text-white text-shadow-lg">
+    <section className="w-screen max-h-screen text-white text-shadow-lg">
       <div className="relative w-full h-[64vh] flex items-baseline-last py-16">
         <Image
           src={placeholder}
@@ -12,9 +12,9 @@ export default function Hero() {
           className="absolute inset-0 z-0 w-full h-full"
         />
 
-        <div className="flex flex-col gap-8 px-8 relative z-10 ">
-          <h1 className="text-6xl">Our Services</h1>
-          <p className="break-normal w-[80vw]">
+        <div className="flex flex-col gap-8 px-8 lg:px-16 relative z-10">
+          <h1 className="text-4xl lg:text-6xl">Our Services</h1>
+          <p className="break-normal w-[80vw] text-sm lg:text-base">
             Saha provides industry-focused services for public and private
             clients. Our experienced staff, combined with our global network,
             allow us to provide the support you need - wherever you need it. We
--- src/components/services/ServiceDisplay.tsx
+++ src/components/services/ServiceDisplay.tsx
@@ -15,41 +15,44 @@ export default function ServiceDisplay({ services }: ServiceProps) {
   const activeService = services.find((s) => s.id === hoveredServiceId);
 
   return (
-    <section className="w-screen h-screen text-black px-8 justify-center">
-      <div className="flex flex-col gap-8 items-center">
-        <h1 className="text-5xl">Overview</h1>
-        <div className="flex flex-row gap-32 items-center">
-          <div className="flex flex-col justify-center text-lg">
-            {services.map((s) => (
-              <Link
-                href="/services"
-                key={s.id}
-                onMouseEnter={() => setHoveredServiceId(s.id)}
-                onMouseLeave={() => setHoveredServiceId(null)}
-                className="border-b p-4 hover:bg-black hover:text-white"
-              >
-                <h2 className="text-md">{s.title}</h2>
-              </Link>
-            ))}
-          </div>
+    <section className="w-screen min-h-screen text-black px-8 flex flex-col gap-8 lg:gap-16 items-center justify-center">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl">Overview</h1>
 
-          <div className="border rounded-4xl w-100 h-100 flex flex-col items-center p-8 gap-4 ">
-            {activeService ? (
-              <>
-                <h1 className="text-2xl">{activeService.title}</h1>
-                <RichText data={activeService.body} />
-              </>
-            ) : (
-              <div className="flex flex-col items-center p-4 gap-4">
-                <h1 className="text-2xl">What We Offer</h1>
-                <ul className="list-disc">
-                  <li>Hover on a service to get a brief overview</li>
-                  <br />
-                  <li>Click on a service to explore further</li>
-                </ul>
-              </div>
-            )}
-          </div>
+      {/* Service List*/}
+      <div className="flex flex-row gap-32 justify-center items-center">
+        <div className="flex flex-col justify-center text-lg">
+          {services.map((s) => (
+            <Link
+              href="/services"
+              key={s.id}
+              onMouseEnter={() => setHoveredServiceId(s.id)}
+              onMouseLeave={() => setHoveredServiceId(null)}
+              className="border-b p-4 hover:bg-black hover:text-white"
+            >
+              <h2 className="text-base lg:text-base">{s.title}</h2>
+            </Link>
+          ))}
+        </div>
+
+        {/* Service Info Box */}
+        <div className="hidden md:flex border rounded-4xl w-100 h-100 flex-col items-center p-8 gap-4 ">
+          {activeService ? (
+            <>
+              <h1 className="text-xl lg:text-2xl text-center">
+                {activeService.title}
+              </h1>
+              <RichText data={activeService.body} />
+            </>
+          ) : (
+            <div className="flex flex-col items-center p-4 gap-4">
+              <h1 className="text-xl lg:text-2xl text-center">What We Offer</h1>
+              <ul className="list-disc">
+                <li>Hover on a service to get a brief overview</li>
+                <br />
+                <li>Click on a service to explore further</li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </section>
