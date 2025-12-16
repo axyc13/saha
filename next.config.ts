bd506f7cbb8fbf1f72d7ba5598113ad7a7d941bc
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-12-16 18:13:47 +1300

    fix: deployment issue

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.externals.push("thread-stream");
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default withPayload(nextConfig);
