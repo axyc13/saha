8c058cd299d2e4cb6eee270a096db82c007a162e
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-12-16 19:04:51 +1300

    fix: deployment issues new try

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pino", "thread-stream", "pino-pretty"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        pino: "commonjs pino",
+        "thread-stream": "commonjs thread-stream",
+        "pino-pretty": "commonjs pino-pretty",
+      });
+    }
+    return config;
+  },
 };
 
 export default withPayload(nextConfig);
