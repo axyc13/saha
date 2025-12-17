273ff058012e8f8515bfef5320b065f54fc5e16d
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-12-17 16:42:32 +1300

    fix: deployment issues with turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["pino", "thread-stream", "pino-pretty"],
-  },
+  serverExternalPackages: ["pino", "thread-stream", "pino-pretty"],
 };
 
 export default withPayload(nextConfig);
