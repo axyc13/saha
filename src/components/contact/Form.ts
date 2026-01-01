582dccc17ae184ae675cc55860ecf63bc9c83ae4
Andrew Chen <ache337@aucklanduni.ac.nz>
2026-01-01 17:07:08 +1300

    feat: reworked email sending

--- src/components/contact/Form.ts
+++ src/components/contact/Form.ts
@@ -1,8 +1,6 @@
 "use server";
 
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 export async function sendFormEmail(formData: FormData) {
   const topic = formData.get("topic");
@@ -12,36 +10,45 @@ export async function sendFormEmail(formData: FormData) {
   const phone = formData.get("phone");
   const message = formData.get("message");
 
-  await resend.emails.send({
-    from: "test@saha.co.nz",
-    to: "test@saha.co.nz",
-    subject: `New message: ${topic}`,
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_APP_PASSWORD,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Saha Contact Form" <${process.env.GMAIL_USER}>`,
+    to: "satyamsaha.ca@gmail.com",
+    replyTo: email?.toString(),
+    subject: `[Contact Form]: New Query: ${topic}`,
     text: `
-      Topic: ${topic}
-      First Name: ${first}
-      Last Name: ${last}
-      Email: ${email}
-      Phone: ${phone}
-      Message:
-      ${message}
+Topic: ${topic}
+Name: ${first} ${last}
+Email: ${email}
+Phone: ${phone}
+
+Message:
+${message}
     `,
     html: `
       <div style="font-family: Arial, sans-serif; padding: 20px;">
-      <h2>New Contact Form Submission</h2>
+        <h2>There is a new form submission:</h2>
 
-      <p><strong>Topic:</strong> ${topic}</p>
-      <p><strong>Name:</strong> ${first} ${last}</p>
-      <p><strong>Email:</strong> ${email}</p>
-      <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Topic:</strong> ${topic}</p>
+        <p><strong>Name:</strong> ${first} ${last}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
 
-      <h3>Message:</h3>
-      <p style="white-space: pre-wrap;">${message}</p>
+        <h3>Message:</h3>
+        <p style="white-space: pre-wrap;">${message}</p>
 
-      <hr />
+        <hr />
 
-      <footer style="margin-top: 20px; font-size: 12px; color: #666;">
+        <footer style="margin-top: 20px; font-size: 12px; color: #666;">
           Sent from saha.co.nz contact form
-      </footer>
+        </footer>
       </div>
     `,
   });
